d2cbc7b0dd89cddb7cdba2b30717be1d16139aca
Ebrukarademir <ebrukarademir03@gmail.com>
2025-01-25 02:32:57 +0300

    v1

---
+++ Assets/Eberu/BubbleShooter.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class BubbleShooter : MonoBehaviour
+{
+    public ParticleSystem bubbleParticle; // Bubble Particle System'ini bağlamak için
+    public int maxShots = 3; // Toplam atış hakkı
+    private int currentShots = 0; // Kullanılan atış hakkı
+
+    void Update()
+    {
+        // Space tuşuna basıldığında ve atış hakkı varsa
+        if (Input.GetKeyDown(KeyCode.Space) && currentShots <= maxShots)
+        {
+            bubbleParticle.Play(); // Bubble'ı ateşle
+            currentShots++; // Atış sayısını artır
+        }
+    }
+}
