5db16792c17e8dac73063045f25900989b9bda36
boranblt <mehmetdotboran@gmail.com>
2025-01-25 01:33:05 +0300

    asas

--- Assets/Boran/HealthController.cs
+++ Assets/Boran/HealthController.cs
@@ -1,18 +1,41 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Slider healthBar; // Health bar UI
+    public int maxHealth = 100;
+    private int currentHealth;
+
     void Start()
     {
-        
+        currentHealth = maxHealth;
+        UpdateHealthBar();
     }
 
-    // Update is called once per frame
-    void Update()
+    void UpdateHealthBar()
     {
-        
+        healthBar.value = currentHealth;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("A"))
+        {
+            // A objesine çarparsa can kazan
+            ChangeHealth(10);
+        }
+        else if (collision.gameObject.CompareTag("B"))
+        {
+            // B objesine çarparsa can kaybet
+            ChangeHealth(-10);
+        }
+    }
+
+    void ChangeHealth(int amount)
+    {
+        currentHealth += amount;
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+        UpdateHealthBar();
     }
 }
